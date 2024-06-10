e7c0c907dcb905937d4f4d609876af2ef84a2134
Jhansen Barreto <jhansen.barreto@ftc.edu.br>
2024-06-09 22:30:19 -0300

    :lipstick: Título DIGIVICE colorido e animado

--- angular-blog/src/app/data/fakeDatabase.ts
+++ angular-blog/src/app/data/fakeDatabase.ts
@@ -3,7 +3,7 @@ export const fakeDB = [
         "id": "289",
         "title": "Agumon",
         "imageURL": "https://digimon-api.com/images/digimon/w/Agumon.png",
-        "description": "A Reptile Digimon with an appearance resembling a small dinosaur,it has grown and become able to walk on two legs. Its strength is weak as it is still in the process of growing, but it has a fearless and rather ferocious personality. Hard, sharp claws grow from both its hands and feet, and their power is displayed in battle. It also foreshadows an evolution into a great and powerful Digimon. Its Special Move is spitting a fiery breath from its mouth to attack the opponent (Baby Flame)."
+        "description": "A Reptile Digimon with an appearance resembling a small dinosaur, it has grown and become able to walk on two legs. Its strength is weak as it is still in the process of growing, but it has a fearless and rather ferocious personality. Hard, sharp claws grow from both its hands and feet, and their power is displayed in battle. It also foreshadows an evolution into a great and powerful Digimon. Its Special Move is spitting a fiery breath from its mouth to attack the opponent (Baby Flame)."
     },
     {
         "id": "682",
@@ -27,7 +27,7 @@ export const fakeDB = [
         "id": "114",
         "title": "Patamon",
         "imageURL": "https://digimon-api.com/images/digimon/w/Patamon.png",
-        "description": "A Mammal Digimon characterized by its large ears. It is able to fly through the air by using them as large wings, but because it only goes at a speed of 1 km / h, it is said that it is definitely faster walking.However, it's very popular because its appearance is cute when it is desperately flying(though it seems that it doesn't feel the same). Due to its extremely obedient personality, its trainers are well- defended.Also, even though Patamon doesn't wear a Holy Ring, it is able to exhibit its hidden holy power, and it appears to have inherited the genes of ancient Digimon. Its Special Moves are sucking in air then spewing an air shot out in one burst(Air Shot), and striking the opponent with its large ears(Hane Binta)."
+        "description": "A Mammal Digimon characterized by its large ears. It is able to fly through the air by using them as large wings, but because it only goes at a speed of 1 km/h, it is said that it is definitely faster walking. However, it's very popular because its appearance is cute when it is desperately flying (though it seems that it doesn't feel the same). Due to its extremely obedient personality, its trainers are well-defended. Also, even though Patamon doesn't wear a Holy Ring, it is able to exhibit its hidden holy power, and it appears to have inherited the genes of ancient Digimon. Its Special Moves are sucking in air then spewing an air shot out in one burst (Air Shot), and striking the opponent with its large ears (Hane Binta)."
     },
     {
         "id": "666",
@@ -46,5 +46,11 @@ export const fakeDB = [
         "title": "Airdramon",
         "imageURL": "https://digimon-api.com/images/digimon/w/Airdramon.png",
         "description": "A Mythical Beast Digimon that grew gigantic wings. It's a highly valued monster due to it being said to have an existence close to that of a god. It specializes in mid-air attacks, its roar calls forth storms, and it causes giant tornadoes by flapping its wings. Its personality is considerably brutal, but it has great intelligence. However, being employed by an ordinary Tamer doesn't seem possible. Its Special Move is flapping its enormous wings to send out a sharp vacuum blade, (Spinning Needle)."
+    },
+    {
+        "id": "16",
+        "title": "Gabumon",
+        "imageURL": "https://digi-api.com/images/digimon/w/Gabumon.png",
+        "description": "Although it is covered by a fur pelt, it is clearly a Reptile Digimon. Due to its extremely timid and shy personality, it always gathers up the data which Garurumon leaves behind, and shapes it into a fur pelt to wear. Because it is wearing the fur pelt of Garurumon, who is feared by other Digimon, it fills the role of safeguarding itself as a result of protecting its body. When it is wearing the fur pelt, its personality does a complete 180° shift. Its Special Move is 'Petit Fire'."
     }
 ]
